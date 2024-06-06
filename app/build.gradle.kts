b8dad509835effaa22c29f6711ba4d063a431492
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2024-06-06 20:35:24 +0200

    target sdk upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     defaultConfig {
         applicationId = "com.example.intervaltimer"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
